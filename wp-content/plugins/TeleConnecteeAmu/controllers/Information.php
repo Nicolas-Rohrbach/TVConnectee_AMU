ea35b5d479ef7d68cafcc45f084158cd4b5a5999
lea-arnaud <lea-arnaud@etu.univ-amu.fr>
2019-04-17 12:09:46 +0200

    Ajout addInformation (WIP)

--- wp-content/plugins/TeleConnecteeAmu/controllers/Information.php
+++ wp-content/plugins/TeleConnecteeAmu/controllers/Information.php
@@ -6,7 +6,16 @@
  * Time: 11:33
  */
 
-class Informations
+class Information
 {
+    private $bdInformation;
+    private $viewInformation;
+
+    public function __construct(){
+        $this->bdInformation = new BdInformation();
+        $this->viewInformation = new ViewInformation();
+    }
+
+
 
 }
--- wp-content/plugins/TeleConnecteeAmu/models/BdInformation.php
+++ wp-content/plugins/TeleConnecteeAmu/models/BdInformation.php
@@ -1,12 +1,44 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: SFW
+ * User: Léa Arnaud
  * Date: 17/04/2019
  * Time: 11:34
  */
+//setlocale(LC_TIME, 'fra_fra');
 
 class BdInformation
 {
 
+    /**
+     * BdInformation constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function addInformation($title, $content, $endDate){
+        global $wpdb;
+
+        $current_user = wp_get_current_user();
+
+        if (isset($current_user)) {
+            $user = $current_user->user_login;
+        }
+        $creationDate = date('Y-m-d H:i:s');
+
+        $wpdb->query(
+            $wpdb->prepare(
+                "INSERT INTO `informations`(`ID_info`, `title`, `author`, `creation_date`, `end_date`, `content`)
+                        VALUES (%d, %s, %s, %s, %s, %s)",
+                null,
+                $title,
+                $user,
+                $creationDate,
+                $endDate,
+                $content
+            )
+        );
+
+    }
 }
